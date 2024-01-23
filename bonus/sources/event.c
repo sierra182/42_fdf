b1b453db2ed9475f77e656caf6cba1cf1d631229
sierra182 <sierra-echo@msn.com>
2024-01-23 18:03:42 +0100

    wip sep

--- bonus/sources/event.c
+++ bonus/sources/event.c
@@ -6,7 +6,7 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 09:05:15 by seblin            #+#    #+#             */
-/*   Updated: 2024/01/23 17:32:37 by seblin           ###   ########.fr       */
+/*   Updated: 2024/01/23 17:56:56 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,44 @@ void	init_event(t_event *event, t_point **pt_arr)
 	event->ty = 0.0;
 	event->tz = -HEIGHT / 1.5;
 	event->scl = get_initial_scale(pt_arr);
-	event->scl_z = 10.0;
+	event->scl_z = 1.0;
 	event->scl_end = 1.0;
 	event->persp = 0.0;
+}
+static void	key_press3(int keycode, t_event *event)
+{
+	if (keycode == 112)
+		event->persp++;
+	else if (keycode == 109)
+		event->persp--;
+	else if (keycode == 120)
+		event->rx++;
+	else if (keycode == 99)
+		event->rx--;
+	else if (keycode == 121)
+		event->ry++;
+	else if (keycode == 117)
+		event->ry--;
+	else if (keycode == 122)
+		event->rz++;
+	else if (keycode == 97)
+		event->rz--;
+}
+
+static void	key_press2(int keycode, t_event *event)
+{
+	if (keycode == 65361)
+		event->tx += 10;
+	else if (keycode == 65363)
+		event->tx -= 10;
+	else if (keycode == 65362)
+		event->ty += 10;
+	else if (keycode == 65364)
+		event->ty -= 10;
+	else if (keycode == 65436)
+		event->tz += 2;
+	else if (keycode == 65438)
+		event->tz -= 2;
 }
 
 int	key_press(int keycode, void *param[])
@@ -36,7 +71,23 @@ int	key_press(int keycode, void *param[])
 
 	event = (t_event *) param[1];
 	event->flag = 1;
-	 if (keycode == 65307)
+	key_press2(keycode, event);
+	key_press3(keycode, event);
+	if (keycode == 102)
+		event->scl_z += 0.1;
+	else if (keycode == 103)
+		event->scl_z -= 0.1;
+	else if (keycode == 115)
+		event->scl += 1;
+	else if (keycode == 100)
+		event->scl -= 1;	
+	else if (keycode == 65432)
+		event->scl_end += 0.1;
+	else if (keycode == 65430)
+		event->scl_end -= 0.1;
+	else if (keycode == 114)
+		init_event(event, (t_point **) param[0]);
+	else if (keycode == 65307)
 		mlx_loop_end(param[2]);
 	return (0);
 }
