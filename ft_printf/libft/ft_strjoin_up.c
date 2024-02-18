0ad20d52ca4dd694bf19a9217f1285590d79c35e
sierra182 <sierra-echo@msn.com>
2024-02-18 21:54:15 +0100

    fix strjoin_up

--- ft_printf/libft/ft_strjoin_up.c
+++ ft_printf/libft/ft_strjoin_up.c
@@ -13,30 +13,14 @@
 #include "libft.h"
 #include <stdlib.h>
 
-static void	free_str(char *s1, char *s2, int s1_free, int s2_free)
-{
-	if (s1_free)
-		free(s1);
-	if (s2_free)
-		free(s2);
-}
-
 char	*ft_strjoin_up(char *s1, char *s2, int s1_free, int s2_free)
 {
 	size_t	s1_len;
 	size_t	s2_len;
 	char	*s3;
 
-	if (!s1)
-		if (s2)
-			return (s2);
-	if (!s2)
-	{
-		if (s1)
-			return (s1);
-		else
-			return (NULL);
-	}
+	if (!s1 || !s2)
+		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	s3 = (char *) malloc(sizeof (char) * (s1_len + s2_len + 1));
@@ -47,5 +31,9 @@ char	*ft_strjoin_up(char *s1, char *s2, int s1_free, int s2_free)
 	while (*s2)
 		*s3++ = *s2++;
 	*s3 = '\0';
-	return (free_str(s1, s2, s1_free, s2_free), (s3 - s2_len - s1_len));
+	if (s1_free)
+		free(s1 - s1_len);
+	if (s2_free)
+		free(s2 - s2_len);
+	return (s3 - s2_len - s1_len);
 }
